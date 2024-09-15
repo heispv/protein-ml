4b3f3e60577ebf2fb3c42c99e2612e0bfb2284c3
heispv <peymanvahidi1998@gmail.com>
2024-09-15 16:11:27 +0200

    Adding data clustering pipeline

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from urllib.parse import quote
 from config import BASE_URL, POSITIVE_QUERY, NEGATIVE_QUERY, BATCH_SIZE, LOG_FILE
 from utils import setup_logging
-from data_fetcher import create_session
-from data_processor import get_pos_dataset, get_neg_dataset
+from pipeline_01_data_fetcher import create_session
+from pipeline_01_data_filterer import get_pos_dataset, get_neg_dataset
+from pipeline_02_data_clusterer import mmseqs
 import logging
 
 def main():
@@ -19,6 +20,9 @@ def main():
     print("\nProcessing negative dataset:")
     get_neg_dataset(neg_url, "neg_filtered_proteins", session)
 
+    print("\nClustering data:")
+    mmseqs()
+    
     logging.info("Protein data processing completed")
     print(f"\nLog file saved as: {LOG_FILE}")
 
