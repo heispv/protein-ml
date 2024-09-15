a0fc754cebc236b8bd03c52570e9c92bc8c3231d
heispv <peymanvahidi1998@gmail.com>
2024-09-15 16:10:34 +0200

    Update file for the data clustering step

--- config.py
+++ config.py
@@ -1,8 +1,17 @@
 import os
 
+DATA_DIR = "data/"
+
+# Data fetching
+FETCHED_DIR = os.path.join(DATA_DIR, "fetched_data/")
 BATCH_SIZE = 500
 BASE_URL = "https://rest.uniprot.org/uniprotkb/search"
 POSITIVE_QUERY = "((taxonomy_id:2759) AND (reviewed:true) AND (length:[40 TO *]) AND (ft_signal_exp:*) AND (fragment:false))"
 NEGATIVE_QUERY = "((existence:1) AND (taxonomy_id:2759) AND (length:[40 TO *]) AND (reviewed:true) AND (fragment:false) AND ((cc_scl_term_exp:SL-0091) OR (cc_scl_term_exp:SL-0191) OR (cc_scl_term_exp:SL-0173) OR (cc_scl_term_exp:SL-0209) OR (cc_scl_term_exp:SL-0204) OR (cc_scl_term_exp:SL-0039)) NOT (ft_signal:*))"
-DATA_DIR = "fetched_data"
-LOG_FILE = os.path.join(DATA_DIR, "protein_data_fetching.log")
+LOG_FILE = os.path.join(DATA_DIR, "pipeline_execution.log")
+
+# Data clustering
+CLUSTER_DIR = os.path.join(DATA_DIR, "clustered_data/")
+MMSEQS_IDENTITY = 0.3
+MMSEQS_COVERAGE = 0.4
+MMSEQS_FILE_PREFIX = f"cluster_results_i{int(100*MMSEQS_IDENTITY)}_c{int(100*MMSEQS_COVERAGE)}"
