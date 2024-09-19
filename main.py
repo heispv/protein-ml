ae22862176bce1fcebd80e2d4a459bbfe93955de
heispv <peymanvahidi1998@gmail.com>
2024-09-19 14:12:25 +0200

    Adding the data analysis functions

--- main.py
+++ main.py
@@ -9,7 +9,19 @@ from pipeline_01_data_filterer import get_pos_dataset, get_neg_dataset
 from pipeline_02_data_clusterer import run_mmseqs
 from pipeline_03_data_splitting import split_fasta_sequences
 from pipeline_04_cross_validation_split import perform_cross_validation_split
-from pipeline_05_filter_tsv import process_all_fasta_files  # Import the function
+from pipeline_05_filter_tsv import process_all_fasta_files
+from pipeline_06_data_analysis import (
+    analyze_protein_length_distribution,
+    analyze_signal_peptide_length_distribution,
+    compare_amino_acid_composition,
+    plot_taxonomic_classification,
+    plot_scientific_name_classification,
+    extract_cleavage_site_sequences
+)
+
+
+
+
 import logging
 import os
 
@@ -81,9 +93,44 @@ def main():
     splitted_data_dir = SPLIT_DIR
     process_all_fasta_files(fetched_data_dir, splitted_data_dir)
     print("Filtering of .tsv files completed.")
+    
+    # Data Analysis for Protein Length
+    print("\nAnalyzing protein length distribution:")
+    figures_output_dir = os.path.join('figures', 'protein_length_dist')
+    analyze_protein_length_distribution(SPLIT_DIR, figures_output_dir, max_length=5000)
+    print("Protein length distribution analysis completed.")
+
+    # Data Analysis for Signal Peptide Length
+    print("\nAnalyzing signal peptide length distribution:")
+    sp_figures_output_dir = os.path.join('figures', 'signal_peptide_length_dist')
+    analyze_signal_peptide_length_distribution(SPLIT_DIR, sp_figures_output_dir, max_length=None)
+    print("Signal peptide length distribution analysis completed.")
+
+    # Comparative Amino Acid Composition Analysis
+    print("\nComparing amino acid composition of SP sequences:")
+    aa_comp_output_dir = os.path.join('figures', 'comparative_aa_composition')
+    compare_amino_acid_composition(SPLIT_DIR, aa_comp_output_dir)
+    print("Amino acid composition comparison completed.")
+
+    # Taxonomic Classification Analysis
+    print("\nAnalyzing taxonomic classification:")
+    taxonomic_output_dir = os.path.join('figures', 'taxonomic_classification')
+    plot_taxonomic_classification(SPLIT_DIR, taxonomic_output_dir)
+    print("Taxonomic classification analysis completed.")
+    
+    # Scientific Name Classification Analysis
+    print("\nAnalyzing scientific name classification:")
+    scientific_name_output_dir = os.path.join('figures', 'scientific_name')
+    plot_scientific_name_classification(SPLIT_DIR, scientific_name_output_dir, num_classifications=7)
+    print("Scientific name classification analysis completed.")
+
+    # Extraction of Cleavage Site Sequences
+    print("\nExtracting cleavage site sequences for weblogo:")
+    msa_output_dir = os.path.join('data', 'seq_logo_weblogo')
+    extract_cleavage_site_sequences(SPLIT_DIR, msa_output_dir)
+    print("Cleavage site sequence extraction for weblogo completed.")
 
     logging.info("Protein data processing completed")
     print(f"\nLog file saved as: {LOG_FILE}")
-
 if __name__ == "__main__":
     main()
